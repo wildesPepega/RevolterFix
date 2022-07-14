353004e8c3297a30bebcbdaad6f706e9bfcc0fea
wildesPepega <68642699+wildesPepega@users.noreply.github.com>
2022-07-15 01:03:51 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games {gta5' }
+
+author 'wildesPepega'
+description 'RevolterFix'
+version '1.0.0'
+ 
+files {
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta'
+}
+ 
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'HANDLING_FILE' 'handling.meta'
